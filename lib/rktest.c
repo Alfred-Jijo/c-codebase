3a0e0af57a743d1df1e06b4f94e0401a1f995bf8
Alfred-Jijo <alfredjijo28@gmail.com>
2024-11-19 11:58:58 +0000

    getrid of test lib dir

--- lib/rktest.c
+++ lib/rktest.c
@@ -1,5 +1,5 @@
 #define DEFINE_RKTEST_IMPLEMENTATION
-#include <rktest/rktest.h>
+#include <rktest.h>
 
 // int main(int argc, const char* argv[]) {
 //     return rktest_main(argc, argv);
--- test/main_test.c
+++ test/main_test.c
@@ -1,5 +1,7 @@
 #include <common.h>
-#include <rktest/rktest.h>
+#include <rktest.h>
+#include <minwindef.h>
+#include <Windows.h>
 
 int
 main (int argc, char **argv) {
