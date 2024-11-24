0d179c06843c39361eb8fe47a8adb109ddb9007f
Alfred-Jijo <alfredjijo28@gmail.com>
2024-11-24 14:09:18 +0000

    remove semicolon in dsa.h for do while loop

--- include/DSA.h
+++ include/DSA.h
@@ -104,7 +104,7 @@ array_ensure_capacity(void *a, size_t item_count, size_t item_size) {
         h->length -= 1; \
         memcpy(ptr, last, sizeof(*a)); \
     } \
-} while (0);
+} while (0)
 
 #define array_pop_back(a) (array_header(a)->length -= 1)
 
