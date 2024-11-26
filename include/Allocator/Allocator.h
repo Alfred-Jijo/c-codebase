6b60b17c6186a78f934382c5c5635d33f309553d
Alfred-Jijo <alfredjijo28@gmail.com>
2024-11-26 22:50:24 +0000

    move common_alloc back to allocator.h

--- include/Allocator/Allocator.h
+++ include/Allocator/Allocator.h
@@ -6,6 +6,34 @@
 
 #include <common.h>
 
+typedef struct common_allocator {
+    void *(*alloc) (size_t size, void *context);
+    void *(*free) (size_t size, void *ptr, void *context);
+    void *context;
+} Allocator;
+
+
+//! MACROS
+/**
+ * make:
+ * Allocates memory for an array of elements of type T
+ * @param T: The type of elements to allocate
+ * @param n: Number of elements to allocate
+ * @param a: Allocator structure containing allocation functions and context
+ * @return Pointer to the allocated memory, cast to type T*
+ * @note Returns NULL if allocation fails
+ */
+#define make(T, n, a) ((T *)((a).alloc(sizeof(T) * (n), (a).context)))
+
+/**
+ * release:
+ * Releases previously allocated memory using the provided allocator
+ * @param s: Size of the memory block to free in bytes
+ * @param p: Pointer to the memory block to free
+ * @param a: Allocator structure containing free functions and context
+ */
+#define release(s, p, a) ((a).free((s), (p), (a).context))
+
 //! Arena Allocator funcs
 typedef struct arena_allocator {
 	void *base;
--- include/common.h
+++ include/common.h
@@ -26,33 +26,6 @@ typedef uint8_t byte;
 
 // size_t PAGE_SIZE = 0;
 
-typedef struct common_allocator {
-    void *(*alloc) (size_t size, void *context);
-    void *(*free) (size_t size, void *ptr, void *context);
-    void *context;
-} Allocator;
-
-
-//! MACROS
-/**
- * make:
- * Allocates memory for an array of elements of type T
- * @param T: The type of elements to allocate
- * @param n: Number of elements to allocate
- * @param a: Allocator structure containing allocation functions and context
- * @return Pointer to the allocated memory, cast to type T*
- * @note Returns NULL if allocation fails
- */
-#define make(T, n, a) ((T *)((a).alloc(sizeof(T) * (n), (a).context)))
-
-/**
- * release:
- * Releases previously allocated memory using the provided allocator
- * @param s: Size of the memory block to free in bytes
- * @param p: Pointer to the memory block to free
- * @param a: Allocator structure containing free functions and context
- */
-#define release(s, p, a) ((a).free((s), (p), (a).context))
 
 #define KB (1024ULL)
 #define MB (KB * 1024)
